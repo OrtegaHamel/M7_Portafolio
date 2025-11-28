1179f080e3d006658812a53d6cf730d8e8af2a6f
Ortega Hamel <ortega.hamel@gmail.com>
2025-11-27 22:47:12 -0300

    Estructura carpetas de requerimientos

---
+++ m7_portafolio/requerimiento2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Requerimiento2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'requerimiento2'
---
+++ m7_portafolio/requerimiento3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Requerimiento3Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'requerimiento3'
